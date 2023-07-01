9c9f6a437b4669feb219d71133210e2adebc4af8
ASWINKUMAR C M <aswin.trikkur@gmail.com>
2023-07-01 22:43:03 +0530

    changed to typed.js package

--- src/pages/Intro/Intro.jsx
+++ src/pages/Intro/Intro.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Intro.css";
 import { useNavigate } from "react-router";
-import ReactTyped from "react-typed";
+import Typed from "typed.js";
 
 export const Intro = () => {
 	const [visible, setVisible] = useState(false);
@@ -18,9 +18,30 @@ export const Intro = () => {
 		};
 	}, []);
 
+	const el = useRef(null);
+	
+	useEffect(() => {
+		const typed = new Typed(el.current, {
+			
+			strings: ['<h3> Are you tired of searching movies ? </h3> ^1000\n   You are at the right place '], // Strings to display
+		  // Speed settings, try diffrent values untill you get good results
+		//   startDelay: 300,
+		  typeSpeed: 30,
+		  showCursor:false
+		});
+	
+		// Destropying
+		return () => {
+		  typed.destroy();
+		};
+	  }, []);
+
 	return (
+		
+
 		<div className="title-container">
-			<ReactTyped
+			<span className="title1" ref={el}></span>
+			{/* <ReactTyped
 				className="title1"
 				strings={[`Are you tired of searching movies ? `]}
 				typeSpeed={40}
@@ -32,7 +53,7 @@ export const Intro = () => {
 				startDelay={3500}
 				typeSpeed={30}
 				showCursor={false}
-			/>
+			/> */}
 
 			{visible && (
 				<div className="button-container" >
